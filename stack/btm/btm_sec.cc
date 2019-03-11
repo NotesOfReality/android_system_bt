7769a15b7163c967a9953ccb2fc4f97631620fbd
Jakub Pawlowski <jpawlowski@google.com>
2019-03-11 19:22:01 +0100

    DO NOT MERGE Don't persist bonds using sample LTK
    Test: compilation, manual testing
    Bug: 128843052
    Change-Id: I52fd484d42bf87e96dbc9e6456090f231ed48111
    (cherry picked from commit 054dcec1c9b6ac732e7380c5e921407cd316474f)

--- stack/btm/btm_sec.cc
+++ stack/btm/btm_sec.cc
@@ -24,6 +24,7 @@
 
 #define LOG_TAG "bt_btm_sec"
 
+#include <log/log.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
@@ -43,6 +44,8 @@
 #if (LEGACY_BT == FALSE)
     #include "gatt_int.h"
 #endif
+
+#include "bta/dm/bta_dm_int.h"
 
 #define BTM_SEC_MAX_COLLISION_DELAY (5000)
 
@@ -4688,6 +4691,19 @@ void btm_sec_disconnected(uint16_t handle, uint8_t reason) {
 
   BTM_TRACE_EVENT("%s after update sec_flags=0x%x", __func__,
                   p_dev_rec->sec_flags);
+
+  /* Some devices hardcode sample LTK value from spec, instead of generating
+   * one. Treat such devices as insecure, and remove such bonds on
+   * disconnection.
+   */
+  if (is_sample_ltk(p_dev_rec->ble.keys.pltk)) {
+    android_errorWriteLog(0x534e4554, "128437297");
+    LOG(INFO) << __func__ << " removing bond to device that used sample LTK";
+
+    tBTA_DM_MSG p_data;
+    p_data.remove_dev.bd_addr = p_dev_rec->bd_addr;
+    bta_dm_remove_device(&p_data);
+  }
 
 #if (LEGACY_BT == FALSE)
   if (p_dev_rec->sec_state == BTM_SEC_STATE_DISCONNECTING_BOTH) {
